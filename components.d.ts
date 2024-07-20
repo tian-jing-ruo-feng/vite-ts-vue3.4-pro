96470993b56692979e75105ce0bfb951f608b5cb
tjrf <2236117954@qq.com>
2024-07-20 22:44:09 +0800

    ✨ feat: add task category with divider component

--- components.d.ts
+++ components.d.ts
@@ -11,6 +11,7 @@ declare module 'vue' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
     ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElDivider: typeof import('element-plus/es')['ElDivider']
     ElEmpty: typeof import('element-plus/es')['ElEmpty']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
--- src/views/todo/tasks.vue
+++ src/views/todo/tasks.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="tasks">
     <ul v-if="tasks?.length">
-      <task-item
-        v-for="task in tasks"
-        :key="task.id"
-        :task="task"
-        @delete-task="handleDeleteTask"
-        @change-task-state="handleChangeTaskState"
-      ></task-item>
+      <li
+        class="task-group"
+        v-for="state in Object.keys(TaskStates)"
+        :key="state"
+      >
+        <el-divider content-position="left">{{ TaskStates[state as TaskState] }}</el-divider>
+        <div v-if="getTasksByState(state as TaskState)">
+          <task-item
+            v-for="task in getTasksByState(state as TaskState)"
+            :key="task.id"
+            :task="task"
+            @delete-task="handleDeleteTask"
+            @change-task-state="handleChangeTaskState"
+          ></task-item>
+        </div>
+        <div v-else>
+          <el-empty :image-size="50" description=" " style="height:50px; padding: 0;" />
+        </div>
+      </li>
     </ul>
     <div v-else style="text-align: center">
       <el-empty :image-size="200" description="暂无任务" />
@@ -16,7 +28,8 @@
 </template>
 
 <script setup lang="ts">
-import taskItem, { type Task, type TaskUpdated } from './taskItem.vue'
+import { TASKS_ARCHIVE, TASKS_DONE, TASKS_TODO } from '../../consts';
+import taskItem, { type Task, type TaskUpdated, type TaskState } from './taskItem.vue'
 export type TasksArr = Task[]
 interface Props {
   tasks: TasksArr
@@ -30,6 +43,19 @@ const emit = defineEmits<{
   remove: [id: string]
   update: [updateTask: TaskUpdated]
 }>()
+
+type ST = {
+  [key in TaskState]: string
+}
+const TaskStates: ST = {
+  [TASKS_TODO]: '计划任务',
+  [TASKS_DONE]: '完成任务',
+  [TASKS_ARCHIVE]: '归档任务'
+}
+
+const getTasksByState = (state: TaskState) => {
+  return props.tasks.filter(task => task.state === state)
+}
 
 const handleDeleteTask = (taskId: string) => {
   emit('remove', taskId)
