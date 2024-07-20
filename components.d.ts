a1d9e22e08662b8a548fa3b08d01d3cba1392b5c
hepeng <2236117954@qq.com>
2024-07-20 14:09:49 +0800

    ✨ feat: adjust todo input with elementui form which can validate input string

--- components.d.ts
+++ components.d.ts
@@ -12,6 +12,8 @@ declare module 'vue' {
     ElCard: typeof import('element-plus/es')['ElCard']
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElEmpty: typeof import('element-plus/es')['ElEmpty']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElHeader: typeof import('element-plus/es')['ElHeader']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
--- src/views/todo/index.vue
+++ src/views/todo/index.vue
@@ -2,15 +2,27 @@
   <div class="todo">
     <h2 class="title">TODO LIST</h2>
     <div class="add-area">
-      <el-input
-        ref="inputTask"
-        class="input-task"
-        v-model="task"
-        @change="addTask"
-      ></el-input>
-      <el-button class="add-button" @click="addTask">
-        <el-icon color="green"><ep-plus></ep-plus></el-icon>
-      </el-button>
+      <el-form
+        :inline="true"
+        :model="form"
+        :rules="rules"
+        hide-required-asterisk
+        ref="formRef"
+        class="demo-form-inline"
+      >
+        <el-form-item label="添加任务：" prop="task">
+          <el-input
+            ref="inputTask"
+            class="input-task"
+            clearable
+            v-model.trim="form.task"
+            @change="addTask"
+          ></el-input>
+          <el-button class="add-button" @click="addTask">
+            <el-icon color="green"><ep-plus></ep-plus></el-icon>
+          </el-button>
+        </el-form-item>
+      </el-form>
     </div>
     <!-- operation button intro -->
     <el-scrollbar :height="height">
@@ -20,31 +32,56 @@
 </template>
 
 <script setup lang="ts">
-import { ElMessage } from 'element-plus'
+import { FormInstance, FormRules } from 'element-plus'
 import Tasks, { type TasksArr } from './tasks.vue'
 import { type Task } from './taskItem.vue'
 import { TASKS } from '../../consts'
 import useTodo from '../../hooks/useTodo'
+interface Form {
+  task: string
+}
+
 let id = 0
 
 const { getItem, setItem } = useTodo()
-const task = ref('')
+
 const tasks = ref<TasksArr>([])
 const inputTask = ref<HTMLInputElement>()
 const height = ref(400)
+const formRef = ref<FormInstance>()
+const form: Form = reactive({
+  task: ''
+})
+const validateTask = (rule: any, value: string, callback: any) => {
+  if (value === '') {
+    callback(new Error('请输入内容！'))
+    setTimeout(() => {
+      formRef.value?.resetFields()
+    }, 3000)
+  } else {
+    callback()
+  }
+}
+const rules = reactive<FormRules<typeof form>>({
+  task: {
+    required: true,
+    validator: validateTask,
+    trigger: 'change'
+  }
+})
 
 const addTask = () => {
-  if (task.value) {
-    tasks.value.unshift({
-      name: task.value,
-      id: id++
-    })
-    task.value = ''
-    // save tasks in localStorage
-    setItem(tasks.value)
-  } else {
-    ElMessage.warning('请输入内容！')
-  }
+  formRef.value?.validate((isValid) => {
+    if (isValid) {
+      tasks.value.unshift({
+        name: form.task,
+        id: id++
+      })
+      form.task = ''
+      // save tasks in localStorage
+      setItem(tasks.value)
+    }
+  })
 }
 const removeTask = (id: number) => {
   const taskIndex = tasks.value.findIndex((task: Task) => task.id === id)
