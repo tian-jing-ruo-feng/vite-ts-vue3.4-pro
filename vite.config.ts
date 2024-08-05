65bbbf0141d1d7a9d088b4a70350e2c12d08adb9
hepeng <2236117954@qq.com>
2024-08-05 09:35:19 +0800

    ci

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-ts-vue3.4-pro/',
+  base: '',
   build: {
     outDir: 'docs'
   },
