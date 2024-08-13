14207741cd623993581c19e677e0baccc4a50646
hepeng <2236117954@qq.com>
2024-08-13 17:09:27 +0800

    🌈 style: modify router push path

--- src/layout/Menu.vue
+++ src/layout/Menu.vue
@@ -1,37 +1,50 @@
 <template>
-  <div class="menu-list">
-    <el-menu
-      :default-active="active"
-      class="el-menu-demo"
-      mode="horizontal"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-      @select="handleSelect"
-    >
-      <el-menu-item
-        v-for="(route, index) in curRoutes"
-        :key="`router${index}`"
-        :index="route.name"
-      >
-        <!-- <router-link :to="route.path">{{ route.name }}</router-link> -->
-        {{ route.name }}
-      </el-menu-item>
-    </el-menu>
-  </div>
+	<div class="menu-list">
+		<el-menu
+			:default-active="active"
+			class="el-menu-demo"
+			mode="horizontal"
+			background-color="#545c64"
+			text-color="#fff"
+			active-text-color="#ffd04b"
+			@select="handleSelect"
+		>
+			<el-menu-item
+				v-for="(route, index) in curRoutes"
+				:key="`router${index}`"
+				:index="route.name"
+			>
+				<!-- <router-link :to="route.path">{{ route.name }}</router-link> -->
+				{{ route.name }}
+			</el-menu-item>
+		</el-menu>
+	</div>
 </template>
 
 <script setup lang="ts">
 import { routes } from '../router/index'
+
 const $router = useRouter()
+const $route = useRoute()
 const curRoutes = routes[0].children || []
 const firstRoute = curRoutes[0]
 const active = ref(firstRoute.name)
 $router.push({ name: active.value })
-const handleSelect = (key: string, keyPath: string[]) => {
-  active.value = key
-  $router.push(active.value)
+const handleSelect = (key: string, keyPath?: string[]) => {
+	active.value = key
+	$router.push(`/${active.value}`)
 }
+const handleLoad = () => {
+	console.log($route)
+	console.log(active.value)
+	handleSelect($route.fullPath)
+}
+onMounted(() => {
+	window.addEventListener('beforeunload', handleLoad)
+})
+onBeforeMount(() => {
+	window.removeEventListener('beforeunload', handleLoad)
+})
 </script>
 
 <style scoped></style>
