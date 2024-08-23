cb16e2d7035ed85a1b8b48375771419d629de6ac
hepeng <2236117954@qq.com>
2024-08-23 14:05:11 +0800

    ✨ feat: provide confit provider to switch language global

--- components.d.ts
+++ components.d.ts
@@ -16,6 +16,7 @@ declare module 'vue' {
     ElCard: typeof import('element-plus/es')['ElCard']
     ElCollapse: typeof import('element-plus/es')['ElCollapse']
     ElCollapseItem: typeof import('element-plus/es')['ElCollapseItem']
+    ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDivider: typeof import('element-plus/es')['ElDivider']
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <RouterView></RouterView>
+	<el-config-provider :locale="locale">
+		<RouterView></RouterView>
+	</el-config-provider>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { zhCn, en } from 'element-plus/es/locales.mjs'
+
+const language = ref('zh-cn')
+const locale = computed(() => (language.value === 'zh-cn' ? zhCn : en))
+
+// 中英文切换
+const toggle = () => {
+	language.value = language.value === 'zh-cn' ? 'en' : 'zh-cn'
+}
+</script>
 
 <style scoped></style>
