6650fcd620e738586a922490acc140b737aa607b
hepeng <2236117954@qq.com>
2024-09-14 17:51:32 +0800

    ✨ feat: add lunar festival in calendar component

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,9 +3,21 @@
 </template>
 
 <script setup lang="ts">
+import { getHolidaysInRange, getLunarDate, getDayDetail } from 'chinese-days'
 import { useTasksStore } from '@/store/tasks'
 // * use scss :export {} variables, the scss files must end with 'module.scss'
 import stateStyles from '@/style/state.module.scss'
+
+interface Holiday {
+	year: number
+	month: number
+	day: number
+	title: string
+	onClick?: object
+	onClickUrl?: string
+	backgroundColor?: string
+	textColor?: string
+}
 
 const { tasks } = toRefs(useTasksStore())
 
@@ -18,18 +30,52 @@ nextTick(() => {
 			// eslint-disable-next-line no-underscore-dangle
 			...window.__TRANSLATION_OPTIONS,
 			dayHeaderNames: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+			holidays: [],
 			views: {
 				fullMonth: {
 					maximumEventsPerDayDisplay: 0
 				}
 			},
 			events: {
-				onEventUpdated: (newEvent: any) => {
+				onEventUpdated(newEvent: any) {
 					console.log(newEvent, 'onEventUpdated')
+				},
+				onSetDate(date: Date) {
+					console.log('datePicker change', date)
+				},
+				onNextMonth(arg: any) {
+					console.log(arg, 'onNextMonth')
 				}
 			}
 		}
 	)
+	// console.log(
+	// 	calendarInstance.getCurrentDisplayDate().getFullYear(),
+	// 	'当前时间'
+	// )
+	const curDisplayYear = calendarInstance.getCurrentDisplayDate().getFullYear()
+	const getHolidaysByYear = (year: number) => {
+		const start = `${year}-01-01`
+		const end = `${year}-12-31`
+		return getHolidaysInRange(start, end, false)
+			.map(date => getDayDetail(date))
+			.map(item => {
+				// console.log(item, 'holiday item')
+				const dateArr = item.date.split('-')
+				const month = Number(dateArr[1])
+				const day = Number(dateArr[2])
+				const title = item.name
+				return {
+					year,
+					month,
+					day,
+					title,
+					backgroundColor: '#f0f9eb',
+					textColor: '#F56C6C'
+				}
+			})
+	}
+
 	// Search Options allow you to customize how Calendar.js Search dialog will function. You can set them manually as follows:
 	calendarInstance.setSearchOptions({
 		matchCase: false
@@ -59,7 +105,9 @@ nextTick(() => {
 			color: stateStyles[`state${eventState(state!)}`]
 		}
 	})
-	calendarInstance.addEvents(events)
+	calendarInstance
+		.addEvents(events)
+		.addHolidays(getHolidaysByYear(curDisplayYear))
 })
 </script>
 
