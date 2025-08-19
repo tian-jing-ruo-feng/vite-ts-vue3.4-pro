ca3cd136da93bad38662eeea3e3bb83357a086fb
tjrf <2236117954@qq.com>
2025-08-19 18:01:17 +0800

    ✨ feat(views/todo/taskGroup): 优化分组列表

--- src/views/todo/TaskGroup.vue
+++ src/views/todo/TaskGroup.vue
@@ -1,60 +1,91 @@
 <template>
-	<div class="task-group">
-		<div v-for="(tag, index) in tags" :key="`tag-${index}`" class="tag-wrapper">
-			<el-tag
-				size="large"
-				class="task-tag"
-				v-if="!tag.isDeleted"
-				:style="{ '--tag-color': tag.color }"
-				:class="{ 'is-editing': tag.edited }"
-				:disable-transitions="false"
-				:checked="tag.checked"
-				:effect="tag.checked ? 'dark' : 'light'"
-				@click="handleTagClick(tag)"
-				@dblclick="handleDoubleClick(tag)"
-				@close="removeTag(tag)"
-			>
-				<span class="tag-name" v-if="!tag.edited">{{ tag.name }}</span>
-				<el-input
-					v-else
-					ref="taskEditedRef"
-					v-model="tag.name"
-					@keyup.enter="handleEditTagChange(tag)"
-					@blur="handleEditTagChange(tag)"
-				></el-input>
-				<el-popconfirm
-					width="100"
-					confirm-button-text="确认"
-					cancel-button-text="取消"
-					icon-color="#F56C6C"
-					title="确认删除分组?"
-					v-if="!!index && !tag.edited"
-					:icon="InfoFilled"
-					@confirm="removeTag(tag)"
+	<div class="task-group-container">
+		<el-scrollbar class="tags-scrollbar" always>
+			<div class="tags-wrapper">
+				<div
+					v-for="(tag, index) in tags"
+					:key="`tag-${index}`"
+					class="tag-wrapper"
 				>
-					<template #reference>
-						<el-icon class="del-button">
-							<Delete></Delete>
-						</el-icon>
-					</template>
-				</el-popconfirm>
-			</el-tag>
+					<el-tag
+						v-if="!tag.isDeleted"
+						size="large"
+						class="task-tag"
+						:style="{ '--tag-color': tag.color }"
+						:class="{
+							'is-editing': tag.edited,
+							'is-active': tag.checked,
+							'is-default': !tag.checked
+						}"
+						:disable-transitions="false"
+						@click="handleTagClick(tag)"
+						@dblclick="handleDoubleClick(tag)"
+					>
+						<el-tooltip
+							v-if="tag.name.length > 6"
+							:content="tag.name"
+							placement="top"
+						>
+							<span v-if="!tag.edited" class="tag-name">{{ tag.name }}</span>
+						</el-tooltip>
+						<span v-if="!tag.edited && tag.name.length <= 6" class="tag-name">{{
+							tag.name
+						}}</span>
+						<el-input
+							v-else
+							ref="taskEditedRef"
+							v-model="tag.name"
+							@keyup.enter="handleEditTagChange(tag)"
+							@blur="handleEditTagChange(tag)"
+						></el-input>
+						<el-popconfirm
+							v-if="!!index && !tag.edited"
+							width="100"
+							confirm-button-text="确认"
+							cancel-button-text="取消"
+							icon-color="#F56C6C"
+							title="确认删除分组?"
+							:icon="InfoFilled"
+							@confirm="removeTag(tag)"
+						>
+							<template #reference>
+								<el-icon class="del-button">
+									<Delete></Delete>
+								</el-icon>
+							</template>
+						</el-popconfirm>
+					</el-tag>
+				</div>
+			</div>
+		</el-scrollbar>
+		<div class="action-buttons">
+			<el-input
+				v-if="inputVisible"
+				ref="InputRef"
+				v-model="inputGroup"
+				class="add-tag-input"
+				placeholder="输入分组名称"
+				@keyup.enter="addTag"
+				@blur="addTag"
+			/>
+			<el-button
+				v-else
+				type="primary"
+				plain
+				class="add-tag-button"
+				@click="showInput"
+			>
+				<el-icon><Plus /></el-icon> 添加分组
+			</el-button>
+			<el-button
+				type="primary"
+				plain
+				class="manage-button"
+				@click="manageGroup"
+			>
+				<el-icon><Setting /></el-icon> 分组管理
+			</el-button>
 		</div>
-		<el-input
-			v-if="inputVisible"
-			ref="InputRef"
-			v-model="inputGroup"
-			class="w-20"
-			@keyup.enter="addTag"
-			@blur="addTag"
-		/>
-		<el-button v-else type="primary" @click="showInput"> + 添加分组 </el-button>
-		<el-button plain type="primary" @click="manageGroup">
-			<el-icon style="margin-right: 8px">
-				<FaCog></FaCog>
-			</el-icon>
-			分组管理
-		</el-button>
 	</div>
 </template>
 
@@ -63,6 +94,7 @@ import { ElInput } from 'element-plus'
 import { v4 as uuidv4 } from 'uuid'
 import { Delete, InfoFilled } from '@element-plus/icons-vue'
 import { useTaskGroupStore, type Tag } from '../../store/taskGroup'
+
 const store = useTaskGroupStore()
 const { add, update, remove, checkTag } = store
 const { tags } = storeToRefs(store)
@@ -164,38 +196,112 @@ const getHexColor = () => {
 }
 </script>
 
-<style scoped lang="scss">
-.task-group {
+<style lang="scss" scoped>
+.task-group-container {
 	display: flex;
-	padding: 10px 4px;
-	border: 1px solid #d4d7de;
+	flex-direction: column;
+	gap: 16px;
+	margin-bottom: 16px;
 
-	.tag-wrapper {
-		&:first-of-type {
-			.tag-name {
+	.tags-scrollbar {
+		max-width: 100%;
+		overflow-x: auto;
+		padding-bottom: 4px;
+
+		.tags-wrapper {
+			display: flex;
+			flex-wrap: wrap;
+			gap: 10px;
+			padding: 8px 12px;
+			background-color: var(--el-fill-color-light);
+			border-radius: var(--el-border-radius-base);
+			box-shadow: 0 1px 4px rgba(0, 0, 0, 0.08);
+			transition: all 0.3s;
+
+			&:hover {
+				box-shadow: 0 2px 12px rgba(0, 0, 0, 0.12);
+			}
+
+			.tag-wrapper {
+				position: relative;
+				transition: transform 0.2s;
+
+				&:hover {
+					transform: scale(1.05);
+				}
+			}
+
+			.task-tag {
+				cursor: pointer;
+				transition: all 0.3s;
+				background-color: var(--tag-color);
+				color: white;
+				border: none;
 				margin-right: 0;
+
+				&:hover {
+					transform: translateY(-2px);
+					box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+				}
+
+				&.is-editing {
+					background-color: transparent;
+					color: var(--el-text-color-regular);
+					border: 1px solid var(--el-border-color);
+				}
+
+				&.is-active {
+					border: 1px solid var(--el-color-primary);
+					box-shadow: 0 0 0 2px rgba(var(--el-color-primary-rgb), 0.3);
+				}
+
+				&.is-default {
+					opacity: 0.8;
+				}
+
+				.tag-name {
+					max-width: 120px;
+					overflow: hidden;
+					text-overflow: ellipsis;
+					white-space: nowrap;
+					display: inline-block;
+					vertical-align: middle;
+				}
+
+				.del-button {
+					margin-left: 8px;
+					cursor: pointer;
+					transition: color 0.2s;
+					&:hover {
+						color: var(--el-color-danger);
+					}
+				}
 			}
 		}
 	}
-	.task-tag {
-		margin-right: 10px;
-		&:hover {
-			cursor: pointer;
-			user-select: none;
+
+	.action-buttons {
+		display: flex;
+		gap: 12px;
+		align-items: center;
+
+		.add-tag-input {
+			width: 180px;
+			transition: all 0.3s;
+
+			&:focus {
+				width: 220px;
+			}
 		}
-		&.is-editing {
-			border: none;
-			padding: 0;
+
+		.add-tag-button,
+		.manage-button {
+			transition: all 0.3s;
+
+			&:hover {
+				transform: translateY(-2px);
+			}
 		}
-		.tag-name {
-			margin-right: 10px;
-		}
-		.del-button {
-			vertical-align: text-top;
-		}
-	}
-	.w-20 {
-		width: 100px;
 	}
 }
 </style>
